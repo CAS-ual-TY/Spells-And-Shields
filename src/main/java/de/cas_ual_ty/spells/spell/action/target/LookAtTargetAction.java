96b89e57c950e08d08d696f045b06f107e322a6c
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2023-01-04 19:11:48 +0100

    Simplify LookAtTargetAction

--- src/main/java/de/cas_ual_ty/spells/spell/action/target/LookAtTargetAction.java
+++ src/main/java/de/cas_ual_ty/spells/spell/action/target/LookAtTargetAction.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import de.cas_ual_ty.spells.registers.TargetTypes;
 import de.cas_ual_ty.spells.spell.action.SpellActionType;
 import de.cas_ual_ty.spells.spell.action.effect.AffectSingleTypeAction;
+import de.cas_ual_ty.spells.spell.context.BuiltinTargetGroups;
 import de.cas_ual_ty.spells.spell.context.SpellContext;
 import de.cas_ual_ty.spells.spell.context.TargetGroup;
 import de.cas_ual_ty.spells.spell.target.EntityTarget;
@@ -23,32 +24,21 @@ public class LookAtTargetAction extends AffectSingleTypeAction<EntityTarget>
     {
         return RecordCodecBuilder.create(instance -> instance.group(
                 activationCodec(),
-                targetsCodec(),
+                sourceCodec(),
                 Codec.DOUBLE.fieldOf(ParamNames.paramDoubleImm("range")).forGetter(LookAtTargetAction::getRange),
                 Codec.FLOAT.fieldOf(ParamNames.paramDoubleImm("bb_inflation")).forGetter(LookAtTargetAction::getBbInflation),
                 SpellsUtil.namedEnumCodec(ClipContext.Block::valueOf).fieldOf("block_clip_context").forGetter(LookAtTargetAction::getBlock),
                 SpellsUtil.namedEnumCodec(ClipContext.Fluid::valueOf).fieldOf("fluid_clip_context").forGetter(LookAtTargetAction::getFluid),
-                Codec.STRING.fieldOf(ParamNames.singleTarget("block_destination")).forGetter(LookAtTargetAction::getBlockDest),
-                Codec.STRING.fieldOf(ParamNames.singleTarget("block_clip_destination")).forGetter(LookAtTargetAction::getBlockClipDest),
-                Codec.STRING.fieldOf(ParamNames.singleTarget("miss_destination")).forGetter(LookAtTargetAction::getMissDest),
-                Codec.STRING.fieldOf(ParamNames.singleTarget("entity_destination")).forGetter(LookAtTargetAction::getEntityDest),
-                Codec.STRING.fieldOf(ParamNames.singleTarget("entity_clip_destination")).forGetter(LookAtTargetAction::getEntityClipDest),
-                Codec.STRING.fieldOf(ParamNames.interactedActivation("block_hit")).forGetter(LookAtTargetAction::getBlockHitActivation),
-                Codec.STRING.fieldOf(ParamNames.interactedActivation("entity_hit")).forGetter(LookAtTargetAction::getEntityHitActivation),
-                Codec.STRING.fieldOf(ParamNames.interactedActivation("miss")).forGetter(LookAtTargetAction::getMissActivation)
-        ).apply(instance, (activation, targets, range, bbInflation, block, fluid, blockDest, blockClipDest, missDest, entityDest, entityClipDest, blockHitActivation, entityHitActivation, missActivation) -> new LookAtTargetAction(type, activation, targets, range, bbInflation, block, fluid, blockDest, blockClipDest, missDest, entityDest, entityClipDest, blockHitActivation, entityHitActivation, missActivation)));
+                Codec.STRING.fieldOf(ParamNames.interactedActivation("block_hit_activation")).forGetter(LookAtTargetAction::getBlockHitActivation),
+                Codec.STRING.fieldOf(ParamNames.interactedActivation("entity_hit_activation")).forGetter(LookAtTargetAction::getEntityHitActivation),
+                Codec.STRING.fieldOf(ParamNames.interactedActivation("miss_activation")).forGetter(LookAtTargetAction::getMissActivation)
+        ).apply(instance, (activation, targets, range, bbInflation, block, fluid, blockHitActivation, entityHitActivation, missActivation) -> new LookAtTargetAction(type, activation, targets, range, bbInflation, block, fluid, blockHitActivation, entityHitActivation, missActivation)));
     }
     
     protected double range;
     protected float bbInflation;
     protected ClipContext.Block block;
     protected ClipContext.Fluid fluid;
-    
-    protected String blockDest;
-    protected String blockClipDest;
-    protected String entityDest;
-    protected String entityClipDest;
-    protected String missDest;
     
     protected String blockHitActivation;
     protected String entityHitActivation;
@@ -59,18 +49,13 @@ public class LookAtTargetAction extends AffectSingleTypeAction<EntityTarget>
         super(type);
     }
     
-    public LookAtTargetAction(SpellActionType<?> type, String activation, String targets, double range, float bbInflation, ClipContext.Block block, ClipContext.Fluid fluid, String blockDest, String blockClipDest, String missDest, String entityDest, String entityClipDest, String blockHitActivation, String entityHitActivation, String missActivation)
+    public LookAtTargetAction(SpellActionType<?> type, String activation, String targets, double range, float bbInflation, ClipContext.Block block, ClipContext.Fluid fluid, String blockHitActivation, String entityHitActivation, String missActivation)
     {
         super(type, activation, targets);
         this.range = range;
         this.bbInflation = bbInflation;
         this.block = block;
         this.fluid = fluid;
-        this.blockDest = blockDest;
-        this.blockClipDest = blockClipDest;
-        this.missDest = missDest;
-        this.entityDest = entityDest;
-        this.entityClipDest = entityClipDest;
         this.blockHitActivation = blockHitActivation;
         this.entityHitActivation = entityHitActivation;
         this.missActivation = missActivation;
@@ -102,31 +87,6 @@ public class LookAtTargetAction extends AffectSingleTypeAction<EntityTarget>
         return fluid;
     }
     
-    public String getBlockDest()
-    {
-        return blockDest;
-    }
-    
-    public String getBlockClipDest()
-    {
-        return blockClipDest;
-    }
-    
-    public String getMissDest()
-    {
-        return missDest;
-    }
-    
-    public String getEntityDest()
-    {
-        return entityDest;
-    }
-    
-    public String getEntityClipDest()
-    {
-        return entityClipDest;
-    }
-    
     public String getBlockHitActivation()
     {
         return blockHitActivation;
@@ -150,19 +110,19 @@ public class LookAtTargetAction extends AffectSingleTypeAction<EntityTarget>
         if(hitResult instanceof BlockHitResult blockHitResult)
         {
             ctx.activate(blockHitActivation);
-            ctx.getOrCreateTargetGroup(blockDest).addTargets(Target.of(entityTarget.getLevel(), blockHitResult.getBlockPos()));
-            ctx.getOrCreateTargetGroup(blockClipDest).addTargets(Target.of(entityTarget.getLevel(), hitResult.getLocation()));
+            ctx.getOrCreateTargetGroup(BuiltinTargetGroups.BLOCK_HIT.targetGroup).addTargets(Target.of(entityTarget.getLevel(), blockHitResult.getBlockPos()));
+            ctx.getOrCreateTargetGroup(BuiltinTargetGroups.HIT_POSITION.targetGroup).addTargets(Target.of(entityTarget.getLevel(), hitResult.getLocation()));
         }
         else if(hitResult instanceof EntityHitResult entityHitResult)
         {
             ctx.activate(entityHitActivation);
-            ctx.getOrCreateTargetGroup(entityDest).addTargets(Target.of(entityHitResult.getEntity()));
-            ctx.getOrCreateTargetGroup(entityClipDest).addTargets(Target.of(entityTarget.getLevel(), hitResult.getLocation()));
+            ctx.getOrCreateTargetGroup(BuiltinTargetGroups.ENTITY_HIT.targetGroup).addTargets(Target.of(entityHitResult.getEntity()));
+            ctx.getOrCreateTargetGroup(BuiltinTargetGroups.HIT_POSITION.targetGroup).addTargets(Target.of(entityTarget.getLevel(), hitResult.getLocation()));
         }
         else
         {
             ctx.activate(missActivation);
-            ctx.getOrCreateTargetGroup(missDest).addTargets(Target.of(entityTarget.getLevel(), hitResult.getLocation()));
+            ctx.getOrCreateTargetGroup(BuiltinTargetGroups.HIT_POSITION.targetGroup).addTargets(Target.of(entityTarget.getLevel(), hitResult.getLocation()));
         }
     }
 }
