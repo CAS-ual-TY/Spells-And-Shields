a19c16f8b81cdc2a84dbaa704c24180baf3987cc
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2023-03-06 20:49:44 +0100

    Add give_item action

---
+++ src/main/java/de/cas_ual_ty/spells/spell/action/item/GiveItemAction.java
@@ -0,0 +1,115 @@
+package de.cas_ual_ty.spells.spell.action.item;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import de.cas_ual_ty.spells.registers.CtxVarTypes;
+import de.cas_ual_ty.spells.registers.SpellActionTypes;
+import de.cas_ual_ty.spells.registers.TargetTypes;
+import de.cas_ual_ty.spells.spell.action.SpellActionType;
+import de.cas_ual_ty.spells.spell.action.base.AffectSingleTypeAction;
+import de.cas_ual_ty.spells.spell.context.SpellContext;
+import de.cas_ual_ty.spells.spell.context.TargetGroup;
+import de.cas_ual_ty.spells.spell.target.ITargetType;
+import de.cas_ual_ty.spells.spell.target.PlayerTarget;
+import de.cas_ual_ty.spells.spell.variable.DynamicCtxVar;
+import de.cas_ual_ty.spells.util.ParamNames;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class GiveItemAction extends AffectSingleTypeAction<PlayerTarget>
+{
+    public static Codec<GiveItemAction> makeCodec(SpellActionType<GiveItemAction> type)
+    {
+        return RecordCodecBuilder.create(instance -> instance.group(
+                activationCodec(),
+                targetCodec(),
+                CtxVarTypes.INT.get().refCodec().fieldOf(ParamNames.paramInt("amount")).forGetter(GiveItemAction::getAmount),
+                CtxVarTypes.INT.get().refCodec().fieldOf(ParamNames.paramInt("damage")).forGetter(GiveItemAction::getDamage),
+                CtxVarTypes.COMPOUND_TAG.get().refCodec().fieldOf(ParamNames.paramCompoundTag("tag")).forGetter(GiveItemAction::getTag),
+                ForgeRegistries.ITEMS.getCodec().fieldOf("item").forGetter(GiveItemAction::getItem)
+        ).apply(instance, (activation, target, amount, damage, tag, item) -> new GiveItemAction(type, activation, target, amount, damage, tag, item)));
+    }
+    
+    public static GiveItemAction make(String activation, String target, DynamicCtxVar<Integer> amount, DynamicCtxVar<Integer> damage, DynamicCtxVar<CompoundTag> tag, Item item)
+    {
+        return new GiveItemAction(SpellActionTypes.GIVE_ITEM.get(), activation, target, amount, damage, tag, item);
+    }
+    
+    protected DynamicCtxVar<Integer> amount;
+    protected DynamicCtxVar<Integer> damage;
+    protected DynamicCtxVar<CompoundTag> tag;
+    protected Item item;
+    
+    public GiveItemAction(SpellActionType<?> type)
+    {
+        super(type);
+    }
+    
+    public GiveItemAction(SpellActionType<?> type, String activation, String targets, DynamicCtxVar<Integer> amount, DynamicCtxVar<Integer> damage, DynamicCtxVar<CompoundTag> tag, Item item)
+    {
+        super(type, activation, targets);
+        this.amount = amount;
+        this.damage = damage;
+        this.tag = tag;
+        this.item = item;
+    }
+    
+    public DynamicCtxVar<Integer> getAmount()
+    {
+        return amount;
+    }
+    
+    public DynamicCtxVar<Integer> getDamage()
+    {
+        return damage;
+    }
+    
+    public DynamicCtxVar<CompoundTag> getTag()
+    {
+        return tag;
+    }
+    
+    public Item getItem()
+    {
+        return item;
+    }
+    
+    @Override
+    public void affectSingleTarget(SpellContext ctx, TargetGroup group, PlayerTarget playerTarget)
+    {
+        ItemStack itemStack = new ItemStack(item);
+        
+        amount.getValue(ctx).ifPresent(amount ->
+        {
+            if(amount >= 0)
+            {
+                itemStack.setCount(amount);
+            }
+        });
+        
+        if(itemStack.isEmpty())
+        {
+            return;
+        }
+        
+        damage.getValue(ctx).ifPresent(damage ->
+        {
+            if(damage >= 0)
+            {
+                itemStack.setDamageValue(damage);
+            }
+        });
+        
+        tag.getValue(ctx).ifPresent(itemStack::setTag);
+        
+        playerTarget.getPlayer().getInventory().add(itemStack);
+    }
+    
+    @Override
+    public ITargetType<PlayerTarget> getAffectedType()
+    {
+        return TargetTypes.PLAYER.get();
+    }
+}
