7fe19a0959ed541ffad31fc85a302816fcc85a0f
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2023-03-25 09:30:17 +0100

    Add get_mana action

---
+++ src/main/java/de/cas_ual_ty/spells/spell/action/mana/GetManaAction.java
@@ -0,0 +1,58 @@
+package de.cas_ual_ty.spells.spell.action.mana;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import de.cas_ual_ty.spells.capability.ManaHolder;
+import de.cas_ual_ty.spells.registers.CtxVarTypes;
+import de.cas_ual_ty.spells.registers.SpellActionTypes;
+import de.cas_ual_ty.spells.registers.TargetTypes;
+import de.cas_ual_ty.spells.spell.action.SpellActionType;
+import de.cas_ual_ty.spells.spell.action.base.GetTargetAttributeAction;
+import de.cas_ual_ty.spells.spell.target.ITargetType;
+import de.cas_ual_ty.spells.spell.target.LivingEntityTarget;
+import de.cas_ual_ty.spells.util.ParamNames;
+
+public class GetManaAction extends GetTargetAttributeAction<LivingEntityTarget>
+{
+    public static Codec<GetManaAction> makeCodec(SpellActionType<GetManaAction> type)
+    {
+        return RecordCodecBuilder.create(instance -> instance.group(
+                activationCodec(),
+                targetCodec(),
+                Codec.STRING.fieldOf(ParamNames.varResult()).forGetter(GetManaAction::getMana)
+        ).apply(instance, (activation, target, mana) -> new GetManaAction(type, activation, target, mana)));
+    }
+    
+    public static GetManaAction make(String activation, String target, String mana)
+    {
+        return new GetManaAction(SpellActionTypes.GET_MANA.get(), activation, target, mana);
+    }
+    
+    protected String mana;
+    
+    public GetManaAction(SpellActionType<?> type)
+    {
+        super(type);
+    }
+    
+    public GetManaAction(SpellActionType<?> type, String activation, String target, String mana)
+    {
+        super(type, activation, target);
+        
+        if(!mana.isEmpty())
+        {
+            addVariableAttribute(e -> ManaHolder.getManaHolder(e.getLivingEntity()).map(mh -> (double) mh.getMana()).orElse(null), CtxVarTypes.DOUBLE.get(), mana);
+        }
+    }
+    
+    public String getMana()
+    {
+        return mana;
+    }
+    
+    @Override
+    public ITargetType<LivingEntityTarget> getAffectedType()
+    {
+        return TargetTypes.LIVING_ENTITY.get();
+    }
+}
