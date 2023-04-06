f31950bba50616308a853ee97a8209b47dca43ae
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2023-04-06 10:17:47 +0200

    Add player_has_items action

---
+++ src/main/java/de/cas_ual_ty/spells/spell/action/item/PlayerHasItemsAction.java
@@ -0,0 +1,141 @@
+package de.cas_ual_ty.spells.spell.action.item;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import de.cas_ual_ty.spells.registers.CtxVarTypes;
+import de.cas_ual_ty.spells.registers.SpellActionTypes;
+import de.cas_ual_ty.spells.registers.TargetTypes;
+import de.cas_ual_ty.spells.spell.action.SpellActionType;
+import de.cas_ual_ty.spells.spell.action.base.AffectTypeAction;
+import de.cas_ual_ty.spells.spell.context.SpellContext;
+import de.cas_ual_ty.spells.spell.context.TargetGroup;
+import de.cas_ual_ty.spells.spell.target.ITargetType;
+import de.cas_ual_ty.spells.spell.target.PlayerTarget;
+import de.cas_ual_ty.spells.spell.variable.DynamicCtxVar;
+import de.cas_ual_ty.spells.util.ParamNames;
+import de.cas_ual_ty.spells.util.SpellsUtil;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerHasItemsAction extends AffectTypeAction<PlayerTarget>
+{
+    public static Codec<PlayerHasItemsAction> makeCodec(SpellActionType<PlayerHasItemsAction> type)
+    {
+        return RecordCodecBuilder.create(instance -> instance.group(
+                activationCodec(),
+                multiTargetsCodec(),
+                Codec.STRING.fieldOf(ParamNames.interactedActivation("to_activate")).forGetter(PlayerHasItemsAction::getToActivate),
+                CtxVarTypes.STRING.get().refCodec().fieldOf(ParamNames.paramString("item")).forGetter(PlayerHasItemsAction::getItem),
+                CtxVarTypes.INT.get().refCodec().fieldOf(ParamNames.paramInt("amount")).forGetter(PlayerHasItemsAction::getAmount),
+                CtxVarTypes.COMPOUND_TAG.get().refCodec().fieldOf(ParamNames.paramCompoundTag("tag")).forGetter(PlayerHasItemsAction::getTag),
+                CtxVarTypes.BOOLEAN.get().refCodec().fieldOf(ParamNames.paramBoolean("must_be_in_hand")).forGetter(PlayerHasItemsAction::getMustBeInHand)
+        ).apply(instance, (activation, multiTargets, toActivate, item, amount, tag, mustBeInHand) -> new PlayerHasItemsAction(type, activation, multiTargets, toActivate, item, amount, tag, mustBeInHand)));
+    }
+    
+    public static PlayerHasItemsAction make(String activation, String multiTargets, String toActivate, DynamicCtxVar<String> item, DynamicCtxVar<Integer> amount, DynamicCtxVar<CompoundTag> tag, DynamicCtxVar<Boolean> mustBeInHand)
+    {
+        return new PlayerHasItemsAction(SpellActionTypes.PLAYER_HAS_ITEMS.get(), activation, multiTargets, toActivate, item, amount, tag, mustBeInHand);
+    }
+    
+    protected String toActivate;
+    protected DynamicCtxVar<String> item;
+    protected DynamicCtxVar<Integer> amount;
+    protected DynamicCtxVar<CompoundTag> tag;
+    protected DynamicCtxVar<Boolean> mustBeInHand;
+    
+    public PlayerHasItemsAction(SpellActionType<?> type)
+    {
+        super(type);
+    }
+    
+    public PlayerHasItemsAction(SpellActionType<?> type, String activation, String multiTargets, String toActivate, DynamicCtxVar<String> item, DynamicCtxVar<Integer> amount, DynamicCtxVar<CompoundTag> tag, DynamicCtxVar<Boolean> mustBeInHand)
+    {
+        super(type, activation, multiTargets);
+        this.toActivate = toActivate;
+        this.item = item;
+        this.amount = amount;
+        this.tag = tag;
+        this.mustBeInHand = mustBeInHand;
+    }
+    
+    public String getToActivate()
+    {
+        return toActivate;
+    }
+    
+    public DynamicCtxVar<String> getItem()
+    {
+        return item;
+    }
+    
+    public DynamicCtxVar<Integer> getAmount()
+    {
+        return amount;
+    }
+    
+    public DynamicCtxVar<CompoundTag> getTag()
+    {
+        return tag;
+    }
+    
+    public DynamicCtxVar<Boolean> getMustBeInHand()
+    {
+        return mustBeInHand;
+    }
+    
+    @Override
+    public void affectTarget(SpellContext ctx, TargetGroup group, PlayerTarget playerTarget)
+    {
+        SpellsUtil.stringToObject(ctx, item, ForgeRegistries.ITEMS).ifPresent(item ->
+        {
+            amount.getValue(ctx).ifPresent(amount ->
+            {
+                mustBeInHand.getValue(ctx).ifPresent(mustBeInHand ->
+                {
+                    Player player = playerTarget.getPlayer();
+                    
+                    List<ItemStack> items;
+                    if(mustBeInHand)
+                    {
+                        items = List.of(player.getMainHandItem(), player.getOffhandItem());
+                    }
+                    else
+                    {
+                        items = new ArrayList<>(player.getInventory().items.size());
+                        items.addAll(player.getInventory().items);
+                        items.addAll(player.getInventory().offhand);
+                    }
+                    
+                    CompoundTag tag = this.tag.getValue(ctx).orElse(null);
+                    
+                    int count = 0;
+                    
+                    for(ItemStack i : items)
+                    {
+                        if(i.getItem() == item && (tag == null || (i.getTag() != null && tag.equals(i.getTag()))))
+                        {
+                            count += i.getCount();
+                        }
+                        
+                        if(count >= amount)
+                        {
+                            ctx.activate(toActivate);
+                            break;
+                        }
+                    }
+                });
+            });
+        });
+    }
+    
+    @Override
+    public ITargetType<PlayerTarget> getAffectedType()
+    {
+        return TargetTypes.PLAYER.get();
+    }
+}
