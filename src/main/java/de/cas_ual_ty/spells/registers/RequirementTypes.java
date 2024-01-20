ae63787df918edee7a5d1a9cb105a8068736a1e8
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2024-01-20 11:47:54 +0100

    list -> min requirement type

--- src/main/java/de/cas_ual_ty/spells/registers/RequirementTypes.java
+++ src/main/java/de/cas_ual_ty/spells/registers/RequirementTypes.java
@@ -21,7 +21,7 @@ public class RequirementTypes
     public static final DeferredHolder<RequirementType<?>, RequirementType<AdvancementRequirement>> ADVANCEMENT = DEFERRED_REGISTER.register("advancement", () -> new RequirementType<>(AdvancementRequirement::new, AdvancementRequirement::makeCodec));
     public static final DeferredHolder<RequirementType<?>, RequirementType<ItemRequirement>> ITEM = DEFERRED_REGISTER.register("item", () -> new RequirementType<>(ItemRequirement::new, ItemRequirement::makeCodec));
     public static final DeferredHolder<RequirementType<?>, RequirementType<ConfigRequirement>> CONFIG = DEFERRED_REGISTER.register("config", () -> new RequirementType<>(ConfigRequirement::new, ConfigRequirement::makeCodec));
-    public static final DeferredHolder<RequirementType<?>, RequirementType<ListRequirement>> LIST = DEFERRED_REGISTER.register("list", () -> new RequirementType<>(ListRequirement::new, ListRequirement::makeCodec));
+    public static final DeferredHolder<RequirementType<?>, RequirementType<MinRequirement>> MIN = DEFERRED_REGISTER.register("min", () -> new RequirementType<>(MinRequirement::new, MinRequirement::makeCodec));
     public static final DeferredHolder<RequirementType<?>, RequirementType<NotRequirement>> NOT = DEFERRED_REGISTER.register("not", () -> new RequirementType<>(NotRequirement::new, NotRequirement::makeCodec));
     public static final DeferredHolder<RequirementType<?>, RequirementType<LearnedRequirement>> LEARNED = DEFERRED_REGISTER.register("learned", () -> new RequirementType<>(LearnedRequirement::new, LearnedRequirement::makeCodec));
     
