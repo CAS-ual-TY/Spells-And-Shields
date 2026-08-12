7996fb75b6fb0c1e2d75af898a179435bb941ec7
CAS_ual_TY <41904979+CAS-ual-TY@users.noreply.github.com>
2026-08-12 18:32:37 +0200

    Remove old DocsGen

--- src/main/java/de/cas_ual_ty/spells/datagen/DataGen.java
+++ src/main/java/de/cas_ual_ty/spells/datagen/DataGen.java
@@ -25,8 +25,7 @@ public class DataGen
     {
         event.getGenerator().addProvider(event.includeClient(), new LangGen(event.getGenerator(), "en_us"));
         event.getGenerator().addProvider(event.includeClient(), new PlayerAnimationsGen(event.getGenerator().getPackOutput()));
-        event.getGenerator().addProvider(event.includeServer(), new DocsGen(event.getGenerator(), SpellsAndShields.MOD_ID, event.getExistingFileHelper()));
-        
+
         event.getGenerator().addProvider(
                 event.includeServer(),
                 (DataProvider.Factory<DatapackBuiltinEntriesProvider>) (PackOutput output) -> new DatapackBuiltinEntriesProvider(
